78a77a49990c1d6d3a7c62c9eadba4a858a65994
MVCRUST <max.crust7@gmail.com>
2025-12-12 14:55:01 +0000

    sorted out final issues, seems to be working now, no time to check full functionality

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/diaryEntries', diaryEntriesRouter);
-app.use(express.static(path.json(__dirname)))
+app.use(express.static(path.join(__dirname)))
 
 module.exports = app;
